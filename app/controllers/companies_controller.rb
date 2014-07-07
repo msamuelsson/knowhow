b94c27466c09f303a5e5c762b7c933d0da4e14fd
Maria Samuelsson <samuelsson.maria@gmail.com>
2014-07-07 16:03:06 +0200

    working example

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,32 +3,50 @@ class CompaniesController < ApplicationController
       	  @companies = Company.all
       end
 
-      #def new
+      def new
         # default: render 'new' template
         #@company = Company.new
-      #end
+      end
 
       def create
         @company = Company.new(company_params)
         #@company = Company.create!(params[:company])
         @company.save
-        #flash[:notice] = "#{@company.compagnia} was successfully created."
+        flash[:notice] = "#{@company.compagnia} was successfully created."
+        redirect_to companies_path
+      end
+
+      def edit
+        @company = Company.find(params[:id])
+      end
+
+      def update
+        @company = Company.find params[:id]
+        if @company.update_attributes(company_params)
+          flash[:notice] = "#{@company.compagnia} was successfully updated."
+          redirect_to companies_path
+        else
+          render 'edit' # note, 'edit' template can access @movie's field values!
+        end
+      end
+
+      def show
+        #id = params[:id]
+        @company = Company.find(params[:id])
+
+
+        # will render app/views/companies/show.html.haml by default
+      end
+      
+      def destroy
+        @company = Company.find(params[:id])
+        @company.destroy
+        flash[:notice] = "Company '#{@company.compagnia}' deleted."
         redirect_to companies_path
       end
 
       private
         def company_params
-          params.require(:company).permit(:compagnia, :provvigione, :autore)
+          params.require(:company).permit(:compagnia, :area, :provvigione, :autore)
         end
-
-      def show
-        id = params[:id]
-        @company = Company.find_by_id(id)
-        if @company == nil
-          flash[:notice] = "Company information could not be found!!!!!!!"
-          redirect_to movies_path
-        end
-        render(:partial => 'company', :object => @company) if request.xhr?
-        # will render app/views/movies/show.<extension> by default
-      end
 end
