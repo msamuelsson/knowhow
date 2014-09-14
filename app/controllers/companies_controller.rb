f12448b2aee467fc415acd73cb26a6dab2562a20
Maria Samuelsson <samuelsson.maria@gmail.com>
2014-09-14 19:32:36 +0200

    Auth bypass, small cosmetics.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,5 @@
 class CompaniesController < ApplicationController
-      before_action :authenticate_user!
+      #before_action :authenticate_user!
   
       helper_method :sort_column, :sort_direction
       def index
